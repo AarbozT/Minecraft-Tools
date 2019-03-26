1060bafdd6382c506dc2a56773a78ffc86519940
Jupisoft <jupitermauro@gmail.com>
2019-03-26 08:09:35 +0100

    Create Ventana_Exportador_Estructuras_Internas.cs

---
+++ Minecraft Tools/Ventana_Exportador_Estructuras_Internas.cs
@@ -0,0 +1,372 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Minecraft_Tools
+{
+    public partial class Ventana_Exportador_Estructuras_Internas : Form
+    {
+        public Ventana_Exportador_Estructuras_Internas()
+        {
+            InitializeComponent();
+        }
+
+        internal enum Tipos_Recetas : int
+        {
+            Crafting_shaped,
+            Crafting_shapeless,
+            Smelting,
+        }
+
+        /// <summary>
+        /// Structure that holds up all the information about a thanked person or organization.
+        /// </summary>
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Recetas
+        {
+            internal string Nombre;
+            internal string Recurso;
+            internal Tipos_Recetas Tipo;
+            internal string Grupo;
+            internal List<string> Lista_Receta;
+            internal List<string> Lista_Recursos;
+            internal double Experiencia;
+            internal double Tiempo;
+
+            internal Recetas(string Nombre, string Recurso, Tipos_Recetas Tipo, string Grupo, List<string> Lista_Receta, List<string> Lista_Recursos, double Experiencia, double Tiempo)
+            {
+                this.Nombre = Nombre;
+                this.Recurso = Nombre.Replace(' ', '_').Replace('~', '_').Replace('=', '_').Replace('+', '_').Replace('-', '_').Replace('\\', '_').Replace('/', '_').Replace(':', '_').Replace('*', '_').Replace('?', '_').Replace('\"', '_').Replace('<', '_').Replace('>', '_').Replace('|', '_').Replace('.', '_');
+                this.Tipo = Tipo;
+                this.Grupo = Grupo;
+                this.Lista_Receta = Lista_Receta;
+                this.Lista_Recursos = Lista_Recursos;
+                this.Experiencia = Experiencia;
+                this.Tiempo = Tiempo;
+            }
+
+            internal static readonly Recetas[] Matriz_Recetas = new Recetas[]
+            {
+                /*new Recetas
+                (
+                    "",
+                    "",
+                    "",
+                    "",
+                    ""
+                ),*/
+            };
+        }
+
+        internal readonly string Texto_Título = "Minecraft Internal Structures Exporter for " + Program.Texto_Usuario + " by Jupisoft";
+        internal bool Variable_Siempre_Visible = false;
+        internal bool Variable_Excepción = false;
+        internal bool Variable_Excepción_Imagen = false;
+        internal int Variable_Excepción_Total = 0;
+        internal bool Variable_Memoria = false;
+        internal static Stopwatch FPS_Cronómetro = Stopwatch.StartNew();
+        internal long FPS_Segundo_Anterior = 0L;
+        internal long FPS_Temporal = 0L;
+        internal long FPS_Real = 0L;
+        internal bool Ocupado = false;
+
+        private void Ventana_Plantilla_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Icon = Program.Icono_Jupisoft.Clone() as Icon;
+                this.Text = Texto_Título;
+                this.WindowState = FormWindowState.Maximized;
+                Ocupado = true;
+                Registro_Cargar_Opciones();
+                Ocupado = false;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Ventana_Plantilla_Shown(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Activate();
+                Temporizador_Principal.Start();
+                /*string[] Matriz_Rutas = Directory.GetFiles(@"", "*.png", SearchOption.TopDirectoryOnly);
+                foreach (string Ruta in Matriz_Rutas)
+                {
+                    File.Move(Ruta, Path.GetDirectoryName(Ruta) + "\\Item_" + Path.GetFileName(Ruta).ToLowerInvariant());
+                }*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Ventana_Plantilla_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            try
+            {
+                Temporizador_Principal.Stop();
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Ventana_Plantilla_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Ventana_Plantilla_DragEnter(object sender, DragEventArgs e)
+        {
+            try
+            {
+                e.Effect = e.Data.GetDataPresent(DataFormats.FileDrop, true) ? DragDropEffects.Copy : DragDropEffects.None;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Plantilla_DragDrop(object sender, DragEventArgs e)
+        {
+            try
+            {
+                if (e.Data.GetDataPresent(DataFormats.FileDrop, true))
+                {
+                    string[] Matriz_Rutas = e.Data.GetData(DataFormats.FileDrop, true) as string[];
+                    if (Matriz_Rutas != null && Matriz_Rutas.Length > 0)
+                    {
+                        foreach (string Ruta in Matriz_Rutas)
+                        {
+                            try
+                            {
+                                if (!string.IsNullOrEmpty(Ruta) && File.Exists(Ruta))
+                                {
+                                    string Extensión = Path.GetExtension(Ruta);
+                                    if (!string.IsNullOrEmpty(Extensión))
+                                    {
+                                        if (string.Compare(Extensión, ".jar", true) == 0) // A Minecraft version.
+                                        {
+
+                                        }
+                                        else // Any NBT structure file.
+                                        {
+
+                                        }
+                                    }
+
+                                    //Minecraft.Información_Niveles Información_Nivel = Minecraft.Información_Niveles.Obtener_Información_Nivel(Ruta);
+                                    SystemSounds.Beep.Play();
+                                    break;
+                                }
+                            }
+                            catch (Exception Excepción)
+                            {
+                                Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null);
+                                continue;
+                            }
+                        }
+                        Matriz_Rutas = null;
+                    }
+                }
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); }
+        }
+
+        private void Ventana_Plantilla_SizeChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                //if (this.WindowState == FormWindowState.Normal) this.WindowState = FormWindowState.Maximized;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Ventana_Plantilla_KeyDown(object sender, KeyEventArgs e)
+        {
+            try
+            {
+                if (!e.Alt && !e.Control && !e.Shift)
+                {
+                    if (e.KeyCode == Keys.Escape)
+                    {
+                        e.Handled = true;
+                        e.SuppressKeyPress = true;
+                        this.Close();
+                    }
+                    else if (e.KeyCode == Keys.Enter)
+                    {
+                        e.Handled = true;
+                        e.SuppressKeyPress = true;
+                    }
+                }
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        internal void Registro_Cargar_Opciones()
+        {
+            try
+            {
+                /*RegistryKey Clave = Registry.CurrentUser.CreateSubKey("Software\\Jupisoft\\Minecraft Tools\\" + Program.Texto_Versión + "\\Template");
+
+                // bool
+                try { Variable_ = bool.Parse((string)Clave.GetValue("Variable_", bool.TrueString)); }
+                catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; Variable_ = true; }
+
+                // int
+                try { Variable_ = (int)Clave.GetValue("Variable_", 0); }
+                catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; Variable_ = 0; }
+                
+                // Correct any bad value after loading:
+                if ((int)Variable_ < 0 || (int)Variable_ > (int)Variables.Variable) Variable_ = Variables.Variable;
+
+                // Apply all the loaded values:
+                ComboBox_Variable_.SelectedIndex = (int)Variable_;
+
+                Menú_Contextual_Variable_.Checked = Variable_;*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        internal void Registro_Guardar_Opciones()
+        {
+            try
+            {
+                /*RegistryKey Clave = Registry.CurrentUser.CreateSubKey("Software\\Jupisoft\\Minecraft Tools\\" + Program.Texto_Versión + "\\Template");
+                string[] Matriz_Nombres = Clave.GetValueNames();
+                if (Matriz_Nombres != null && Matriz_Nombres.Length > 0)
+                {
+                    for (int Índice = 0; Índice < Matriz_Nombres.Length; Índice++)
+                    {
+                        Clave.DeleteValue(Matriz_Nombres[Índice]);
+                    }
+                }
+                Matriz_Nombres = null;
+                
+                // bool
+                try { Clave.SetValue("Variable_", Variable_doDaylightCycle.ToString(), RegistryValueKind.String); }
+                catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+
+                // int
+                try { Clave.SetValue("Tickspeed", (int)Variable_, RegistryValueKind.DWord); }
+                catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        internal void Registro_Restablecer_Opciones()
+        {
+            try
+            {
+                /*RegistryKey Clave = Registry.CurrentUser.CreateSubKey("Software\\Jupisoft\\Minecraft Tools\\" + Program.Texto_Versión + "\\Template");
+                string[] Matriz_Nombres = Clave.GetValueNames();
+                if (Matriz_Nombres != null && Matriz_Nombres.Length > 0)
+                {
+                    for (int Índice = 0; Índice < Matriz_Nombres.Length; Índice++)
+                    {
+                        try { Clave.DeleteValue(Matriz_Nombres[Índice]); }
+                        catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; continue; }
+                    }
+                    Matriz_Nombres = null;
+                }
+                Clave.Close();
+                Clave = null;*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Visor_Ayuda_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana_Visor_Ayuda Ventana = new Ventana_Visor_Ayuda();
+                Ventana.Ayuda = Ventana_Visor_Ayuda.Ayudas.Main_window;
+                Ventana.ShowDialog(this);
+                Ventana.Dispose();
+                Ventana = null;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Acerca_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana_Acerca Ventana = new Ventana_Acerca();
+                Ventana.ShowDialog(this);
+                Ventana.Dispose();
+                Ventana = null;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Depurador_Excepciones_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Ventana_Depurador_Excepciones Ventana = new Ventana_Depurador_Excepciones();
+                Ventana.ShowDialog(this);
+                Ventana.Dispose();
+                Ventana = null;
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Abrir_Carpeta_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                Program.Crear_Carpetas(Program.Ruta_Minecraft);
+                Program.Ejecutar_Ruta(Program.Ruta_Guardado_Minecraft, ProcessWindowStyle.Maximized);
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Actualizar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Copiar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*if (Picture.Image != null)
+                {
+                    Clipboard.SetImage(Picture.Image);
+                    SystemSounds.Asterisk.Play();
+                }*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+
+        private void Menú_Contextual_Guardar_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                /*if (Picture.Image != null)
+                {
+                    Program.Crear_Carpetas(Program.Ruta_Minecraft);
+                    Picture.Image.Save(Program.Ruta_Minecraft + "\\" + Program.Obtener_Nombre_Temporal_Sin_Guiones() + ".png", ImageFormat.Png);
+                    SystemSounds.Asterisk.Play();
+                }*/
+            }
+            catch (Exception Excepción) { Depurador.Escribir_Excepción(Excepción != null ? Excepción.ToString() : null); Variable_Excepción_Total++; Variable_Excepción = true; }
+        }
+    }
+}
