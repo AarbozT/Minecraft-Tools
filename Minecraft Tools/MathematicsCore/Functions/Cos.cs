7d9ee62a3e62822420feeeefdca6214feca7f417
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:31 +0200

    Create Cos.cs

---
+++ Minecraft Tools/MathematicsCore/Functions/Cos.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mathematics.BaseClasses;
+namespace Mathematics.Functions
+{
+    class Cos : FunctionBase
+    {
+       
+
+        public double A { get; private set; }
+        public double N { get; private set; }
+        public double D { get; private set; }
+
+        public Cos()
+        {
+             A = 1;
+             N = 1;
+             D = 0;
+        }
+        public Cos(double a, double n, double d)
+        {
+             A = a;
+             N = n;
+             D = d;
+        }
+
+        public override double Value(double x)
+        {
+            return  A * Math.Cos( N * x +  D);
+        }
+
+        public override double Derivative(double x)
+        {
+            return - N *  A * Math.Sin( N * x +  D);
+        }
+    }
+}
