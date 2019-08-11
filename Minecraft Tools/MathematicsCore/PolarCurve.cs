c5773310589f7b87e6b5a1dfb94b2222a9fae246
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:34 +0200

    Create PolarCurve.cs

---
+++ Minecraft Tools/MathematicsCore/PolarCurve.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mathematics.BaseClasses;
+namespace Mathematics
+{
+  public  sealed  class PolarCurve:PolarCurveBase 
+    {
+        private Func<double, double> mR;
+        private Func<double, double> mDr;
+
+        public PolarCurve(Function r)
+        {
+            mR = r.Value;
+            mDr = r.Derivative;
+        }
+
+        public override double R(double theta)
+        {
+            return mR(theta);
+        }
+
+        public override double Dr(double theta)
+        {
+            return mDr(theta);
+        }
+    }
+}
