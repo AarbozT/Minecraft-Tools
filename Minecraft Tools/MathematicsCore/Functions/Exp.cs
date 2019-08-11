00f32e06a7293894d52e1773311512023db4f1ef
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:32 +0200

    Create Exp.cs

---
+++ Minecraft Tools/MathematicsCore/Functions/Exp.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mathematics.BaseClasses;
+namespace Mathematics.Functions
+{
+  public  class Exp :FunctionBase 
+    {
+
+      public double A { get; private set; }
+      public double K { get; private set; }
+      public double D { get; private set; } 
+
+      public Exp()
+      {
+          A = 1;
+          K = 1;
+          D = 0;
+
+      }
+        public override double Value(double x)
+        {
+           return A * Math.Exp(x * K + D) ;
+        }
+
+        public override double Derivative(double x)
+        {
+            return A * K * Math.Exp(x * K + D);
+        }
+    }
+}
