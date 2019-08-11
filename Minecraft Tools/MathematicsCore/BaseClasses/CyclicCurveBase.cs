e96c92c22daa41abd523aa7570ca2ebf40efb00c
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:20 +0200

    Create CyclicCurveBase.cs

---
+++ Minecraft Tools/MathematicsCore/BaseClasses/CyclicCurveBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mathematics.BaseClasses  
+{
+  public abstract class CyclicCurveBase:CurveBase 
+    {
+      public abstract double CycleStart {get;}
+      public abstract double CycleEnd{ get;}
+    }
+}
