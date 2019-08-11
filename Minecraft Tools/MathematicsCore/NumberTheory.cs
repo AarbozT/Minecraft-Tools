f4948ee7eed41ad43606063f0de324f0e2063cbb
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:33 +0200

    Create NumberTheory.cs

---
+++ Minecraft Tools/MathematicsCore/NumberTheory.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Mathematics
+{
+  public static class NumberTheory
+    {
+      public static int GCD(int a, int b)
+      {
+          int temp;
+          while (b != 0)
+          {
+              temp = b;
+              b = a % b;
+              a = temp;
+          }
+          return a;
+      }
+
+      public static int LCD(int a, int b)
+      {
+          return a * b / GCD(a, b);
+
+
+      }
+    }
+
+    
+}
