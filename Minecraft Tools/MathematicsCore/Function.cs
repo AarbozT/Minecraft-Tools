fa330b223fd4dede300143e8ffae49e3c599e932
Jupisoft <jupitermauro@gmail.com>
2019-08-11 17:12:30 +0200

    Create Function.cs

---
+++ Minecraft Tools/MathematicsCore/Function.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Mathematics.BaseClasses;
+namespace Mathematics
+{
+  public sealed  class Function:FunctionBase
+    {
+        private Func<double, double> mF;
+        private Func<double, double> mDF;
+
+        public Function( Func<double,double> f, Func<double,double> df)
+      {
+          mF = f;
+          mDF = df;
+      }
+
+        public override double Value(double x)
+        {
+            return mF(x);
+        }
+
+        public override double Derivative(double x)
+        {
+            return mDF(x);
+        }
+    }
+}
